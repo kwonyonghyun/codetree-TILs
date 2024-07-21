bdc64c3e1377b18223c4d6e2b0063ac215178292
kwonyonghyun <106018526+kwonyonghyun@users.noreply.github.com>
2024-07-21 18:24:46 +0900

    Title: 여러가지 진수변환, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240721/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(Integer.toString(n,b));
+    }
+}
